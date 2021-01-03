6e81a0c085518a8479a334b45aea4ca1efa91297
vben <anncwb@126.com>
2021-01-03 15:22:54 +0800

    chore: update example

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -1,27 +1,29 @@
-import { defineConfig } from 'vite';
+import { UserConfigExport, ConfigEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 import html from '../src/index';
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    html({
-      title: 'Vite App',
-      minify: true,
-      options: {
-        injectConfig: '<script src="./a.js"></script>',
-      },
-      // tags: [
-      //   {
-      //     tag: 'div',
-      //     attrs: {
-      //       src: './',
-      //     },
-      //     children: '1',
-      //     injectTo: 'body',
-      //   },
-      // ],
-    }),
-  ],
-});
+export default ({ command, mode }: ConfigEnv): UserConfigExport => {
+  return {
+    plugins: [
+      vue(),
+      html({
+        title: 'Vite App',
+        minify: mode === 'production',
+        options: {
+          injectConfig: '<script src="./a.js"></script>',
+        },
+        // tags: [
+        //   {
+        //     tag: 'div',
+        //     attrs: {
+        //       src: './',
+        //     },
+        //     children: '1',
+        //     injectTo: 'body',
+        //   },
+        // ],
+      }),
+    ],
+  };
+};
