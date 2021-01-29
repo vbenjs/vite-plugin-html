07e9041afa5684461ece3757391b31b6687c8da7
vben <anncwb@126.com>
2021-01-29 22:32:25 +0800

    refacotr(minify): ensure  html is minify

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,12 @@
-# 2.0.0-beta.3 (2021-01-06)
+# [2.0.0-rc.1](https://github.com/vbenjs/vite-plugin-html/compare/v2.0.0-beta.2...v2.0.0-rc.1) (2021-01-29)
 
-### ✨ Fix
+### Bug Fixes
 
-- Fix css compression failure
+- css build error ([12cd218](https://github.com/vbenjs/vite-plugin-html/commit/12cd218c3f02267022eed06eea18c8e67d4119ff))
+- fix css compression failure [#1](https://github.com/vbenjs/vite-plugin-html/issues/1) ([b62e99c](https://github.com/vbenjs/vite-plugin-html/commit/b62e99cd809a0a581cbd1e1dae9260d0b35e9abb))
+
+### Features
+
+- inject title to viteHtmlPluginOptions ([3b34151](https://github.com/vbenjs/vite-plugin-html/commit/3b341516cc78c83619d672ab1c5316a4339a92ac))
 
 # 2.0.0-beta.2 (2021-01-03)
-
-### ✨ Features
-
-- Added tag attribute configuration
--- src/minifyHtml.ts
+++ src/minifyHtml.ts
@@ -1,28 +1,67 @@
 import { minify, Options as MinifyOptions } from 'html-minifier-terser';
-import type { Plugin } from 'vite';
+import type { Plugin, ResolvedConfig } from 'vite';
+import fs from 'fs-extra';
+import path from 'path';
+import { normalizePath } from 'vite';
 
 export function minifyHtml(minifyOptions: MinifyOptions | boolean = true): Plugin {
+  let config: ResolvedConfig;
   return {
     name: 'vite:minifyHtml',
-    transformIndexHtml: {
-      enforce: 'post',
-      transform(html: string) {
-        if (!minifyOptions) {
-          return html;
-        }
-        const defaultMinifyOptions = {
-          collapseBooleanAttributes: true,
-          collapseWhitespace: true,
-          minifyCSS: true,
-          minifyJS: true,
-          minifyURLs: true,
-          removeAttributeQuotes: true,
-          removeComments: true,
-          removeEmptyAttributes: true,
-          ...(typeof minifyOptions === 'boolean' ? {} : minifyOptions),
-        };
-        return minify(html, defaultMinifyOptions);
-      },
+
+    configResolved(resolvedConfig: ResolvedConfig) {
+      config = resolvedConfig;
+    },
+    // transformIndexHtml: {
+    //   enforce: 'post',
+    //   transform(html: string) {
+    //     if (!minifyOptions) {
+    //       return html;
+    //     }
+    //     const defaultMinifyOptions = {
+    //       collapseBooleanAttributes: true,
+    //       collapseWhitespace: true,
+    //       minifyCSS: true,
+    //       minifyJS: true,
+    //       minifyURLs: true,
+    //       removeAttributeQuotes: true,
+    //       removeComments: true,
+    //       removeEmptyAttributes: true,
+    //       ...(typeof minifyOptions === 'boolean' ? {} : minifyOptions),
+    //     };
+    //     return minify(html, defaultMinifyOptions);
+    //   },
+    // },
+
+    async closeBundle() {
+      if (!minifyOptions) {
+        return;
+      }
+
+      // Since the html conversion can also be maintained inside the plug-in, it cannot be guaranteed that the subsequent plug-in will compress the html. So it is changed to compress after packaging
+      const defaultMinifyOptions = {
+        collapseBooleanAttributes: true,
+        collapseWhitespace: true,
+        minifyCSS: true,
+        minifyJS: true,
+        minifyURLs: true,
+        removeAttributeQuotes: true,
+        removeComments: true,
+        removeEmptyAttributes: true,
+        ...(typeof minifyOptions === 'boolean' ? {} : minifyOptions),
+      };
+
+      const { root, build } = config;
+      const { outDir } = build;
+      const indexHtmlPath = normalizePath(path.resolve(root, outDir, 'index.html'));
+      if (!fs.existsSync(indexHtmlPath)) {
+        console.log('no such file: ' + indexHtmlPath);
+        return;
+      }
+      let processHtml = fs.readFileSync(indexHtmlPath, 'utf-8');
+      processHtml = minify(processHtml, defaultMinifyOptions);
+
+      fs.writeFileSync(indexHtmlPath, processHtml);
     },
   };
 }
