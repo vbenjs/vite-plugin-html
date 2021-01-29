3297d3cf9b1a421c3a3e812a2146bef6eb4de3f3
vben <anncwb@126.com>
2021-01-29 22:53:30 +0800

    chore: minify on post

--- src/minifyHtml.ts
+++ src/minifyHtml.ts
@@ -4,22 +4,25 @@ import type { Plugin } from 'vite';
 export function minifyHtml(minifyOptions: MinifyOptions | boolean = true): Plugin {
   return {
     name: 'vite:minifyHtml',
-    transformIndexHtml(html: string) {
-      if (!minifyOptions) {
-        return html;
-      }
-      const defaultMinifyOptions = {
-        collapseBooleanAttributes: true,
-        collapseWhitespace: true,
-        minifyCSS: true,
-        minifyJS: true,
-        minifyURLs: true,
-        removeAttributeQuotes: true,
-        removeComments: true,
-        removeEmptyAttributes: true,
-        ...(typeof minifyOptions === 'boolean' ? {} : minifyOptions),
-      };
-      return minify(html, defaultMinifyOptions);
+    transformIndexHtml: {
+      enforce: 'post',
+      transform(html: string) {
+        if (!minifyOptions) {
+          return html;
+        }
+        const defaultMinifyOptions = {
+          collapseBooleanAttributes: true,
+          collapseWhitespace: true,
+          minifyCSS: true,
+          minifyJS: true,
+          minifyURLs: true,
+          removeAttributeQuotes: true,
+          removeComments: true,
+          removeEmptyAttributes: true,
+          ...(typeof minifyOptions === 'boolean' ? {} : minifyOptions),
+        };
+        return minify(html, defaultMinifyOptions);
+      },
     },
   };
 }
