ab4a14575ee2acfe937305c62789901878a1c7f3
Thorn Walli <lammpee@gmail.com>
2023-08-21 10:50:17 +0200

    fix: wrong pages file path usage

--- packages/core/src/htmlPlugin.ts
+++ packages/core/src/htmlPlugin.ts
@@ -295,7 +295,8 @@ export function getPageConfig(
   }
 
   const page = pages.filter((page) => {
-    return path.resolve('/' + page.template) === path.resolve('/' + htmlName)
+    // Path to specified file path, if not to template path
+    return path.resolve('/' + page.filename || page.template) === path.resolve('/' + htmlName)
   })?.[0]
   return page ?? defaultPageOption ?? undefined
 }
@@ -332,7 +333,8 @@ function createRewire(
 
       const excludeBaseUrl = pathname.replace(baseUrl, '/')
 
-      const template = path.resolve(baseUrl, page.template)
+      // Path to specified file path, if not to template path
+      const template = path.resolve(baseUrl, page.filename || page.template)
 
       if (excludeBaseUrl === '/') {
         return template
